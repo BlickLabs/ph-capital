cf8b30310784e3692679a41d9ed7ae6dd1c5d076
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2019-05-01 10:02:54 -0500

    Update styles

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,14 +5,14 @@ var siteNavbar = document.querySelector('.navbar');
 
 //navbar-fixed-top
 function doSomething(scroll_pos) {
-  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-name/index.html' || window.location.pathname === '/project-name/') {
+  // if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/ph-capital/index.html' || window.location.pathname === '/ph-capital/') {
     if (scroll_pos > 30) {
       siteNavbar.classList.add('navbar-scroll');
 
     } else {
       siteNavbar.classList.remove('navbar-scroll');
     }
-  }
+  // }
 }
 
 doSomething();
