7682a17f2061e95594b6cd574d7a3a279758ffa5
Raúl Marty <santor3medio@gmail.com>
2019-07-07 19:09:31 -0500

    #5. Add the contact form integration in contact-form.js file.

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,30 +1,67 @@
+//////////////////////////////////////////
+// Formulario de contacto en español
+//////////////////////////////////////////
+
 $('form[name="contact-form"]').validate({
   name: 'required',
   email: 'required',
-  phone: 'required',
+  message: 'required',
   messages: {
-   name: "Por favor, introduce tu Nombre.",
-   email: "Por favor, introduce un Correo Electrónico.",
-   phone: "Por favor, introduce tu Teléfono."
+    name: "Por favor, introduce tu nombre",
+    email: "Por favor introduce tu correo electrónico",
+    message: "Por favor, introduce tu mensaje"
   },
   submitHandler: function(form) {
-   var data = $('form[name="contact-form"]').serialize();
-   console.log(data);
-   $.ajax({
-     url: 'http://integrations.blick.mx/',
-     method: 'POST',
-     data: data
-   }).done(function(data) {
-     if (parseInt(data) === 1) {
-       alertify.logPosition("bottom right");
-       alertify.success("Correo enviado, gracias por contactarte con nosotros.");
-       $('form[name="contact-form"]')[0].reset();
-     }
-   });
+    var formData = new FormData(document.getElementById('contact-form'));
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://ph.capital/integracion-formularios/contacto-espanol.php',
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false
+    }).done(function() {
+      document.getElementById('contact-form').reset();
+      alertify.success('Se ha enviado el email. Gracias por contactarnos!');
+    });
   },
   invalidHandler: function(event, validator) {
-   var errors = validator.numberOfInvalids();
-   alertify.logPosition("bottom right");
-   alertify.error("Verifica tu información tienes " + errors + " errores.");
+    var errors = validator.numberOfInvalids();
+    alertify.error("Verifica la información. Tienes " + errors + " errores.");
+  }
+});
+
+//////////////////////////////////////////
+// Formulario de contacto en inglés
+//////////////////////////////////////////
+
+$('form[name="contact-form-en"]').validate({
+  name: 'required',
+  email: 'required',
+  message: 'required',
+  messages: {
+    name: "Please, enter your name",
+    email: "Please, enter your email",
+    message: "Please, enter your message",
+  },
+  submitHandler: function(form) {
+    var formData = new FormData(document.getElementById('contact-form-en'));
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://ph.capital/integracion-formularios/contacto-ingles.php',
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false
+    }).done(function() {
+      document.getElementById('contact-form-en').reset();
+      alertify.success('The email has been sent. Thanks for contact us!');
+    });
+  },
+  invalidHandler: function(event, validator) {
+    var errors = validator.numberOfInvalids();
+    alertify.error("Verify yout information. You have " + errors + " errors.");
   }
 });
